595d63e612bd50d14fb1d6ca9a55c11358cdc565
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2024-04-23 15:32:29 -0700

    Add projects section

---
+++ app/sections/Projects/Projects.tsx
@@ -0,0 +1,25 @@
+import { LinksFunction } from "@remix-run/node";
+import * as React from "react";
+
+import FeaturedProjects from "./FeaturedProjects";
+import OtherProjects from "./OtherProjects";
+import styles from "./Projects.css";
+
+export const links: LinksFunction = () => [
+  {
+    rel: "stylesheet",
+    href: styles
+  }
+];
+
+const Projects: React.FC = () => {
+  return (
+    <div className="projects-wrapper">
+      <FeaturedProjects />
+      <div className="mt-14 sm:mt-24"></div>
+      <OtherProjects />
+    </div>
+  );
+};
+
+export default Projects;
