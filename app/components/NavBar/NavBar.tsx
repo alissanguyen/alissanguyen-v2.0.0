59bbdb1189b8711939482ed895094c681f23a364
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2024-04-23 15:38:43 -0700

    Add nav bar

---
+++ app/components/NavBar/NavBar.tsx
@@ -0,0 +1,117 @@
+import { Link, useLocation } from "@remix-run/react";
+import * as React from "react";
+
+import { fixedWidthLayoutClasses, NAVBAR_ID, topLevelLinksOnDesktop } from "~/constants";
+import { useModalContext } from "~/providers/ModalProvider";
+import { useTheme } from "~/providers/ThemeProvider";
+import { SupportedTheme } from "~/types";
+
+import ThemeButton from "../ThemeButton/ThemeButton";
+
+import MobileMenu from "./MobileMenu";
+import NavLink from "./NavLink";
+
+const Navbar: React.FC = () => {
+  const location = useLocation();
+  const { theme } = useTheme();
+  const currentTopLevelRoute = location.pathname.split("/")[1];
+
+  return (
+    <>
+      <div
+        className={fixedWidthLayoutClasses + " mb-10 md:mb-16 lg:mb-20"}
+        id={NAVBAR_ID}
+      >
+        <nav className="px-5vw nav-bar-wrapper py-10 pb-15 w-full mx-auto flex max-w-8xl items-center justify-between">
+          <div>
+            <NavLogo
+              isCurrentRoute={getIsActiveRoute(
+                "/",
+                currentTopLevelRoute === undefined ? "" : currentTopLevelRoute
+              )}
+            />
+          </div>
+
+          <ul className="hidden lg:flex lg:w-1/2 justify-between list-none">
+            {topLevelLinksOnDesktop.map((link) => {
+              return (
+
+                <NavLink
+                  key={link.href}
+                  to={link.href}
+                  isCurrentRoute={getIsActiveRoute(
+                    link.href,
+                    currentTopLevelRoute === undefined
+                      ? ""
+                      : currentTopLevelRoute
+                  )}
+                >
+                    {link.displayName}
+                </NavLink>
+              );
+            })}
+          </ul>
+
+          <div className="flex items-center justify-center">
+            <div className="block lg:hidden">
+              <MobileMenu theme={theme} />
+            </div>
+            <div className="noscript-hidden hidden lg:block">
+              <ThemeButton />
+            </div>
+          </div>
+        </nav>
+      </div>
+    </>
+  );
+};
+
+const getIsActiveRoute = (href: string, currentTopLevelRoute: string) => {
+
+  if (href.startsWith("/#")) {
+    return false;
+  }
+
+  return "/" + currentTopLevelRoute === href;
+};
+
+export default Navbar;
+
+interface Props {
+  isCurrentRoute: boolean;
+}
+const NavLogo: React.FC<Props> = (props) => {
+  const { theme } = useTheme();
+  const { modalIsOpen } = useModalContext();
+
+  const logoText = getLogoClassName(
+    theme,
+    modalIsOpen
+  );
+
+  const IS_CURRENT_ROUTE_CLASSNAME = "text-navBar-linkActive";
+  return (
+    <Link
+      prefetch="intent"
+      to="/"
+      className={`logo focus:outline-none block whitespace-nowrap text-2xl font-medium transition uppercase ${logoText} ${props.isCurrentRoute ? IS_CURRENT_ROUTE_CLASSNAME : null
+        }`}
+    >
+      <span>Alissa Nguyen</span>
+    </Link>
+  );
+};
+
+const getLogoClassName = (
+  theme: SupportedTheme,
+  modalIsOpen: boolean
+) => {
+  if (modalIsOpen) {
+    return theme === SupportedTheme.LIGHT
+      ? "text-gray-500 hover:text-black"
+      : "text-gray-400 hover:text-white";
+  }
+  return theme === SupportedTheme.LIGHT
+    ? "text-gray-500 hover:text-black"
+    : "text-gray-400 hover:text-white";
+};
---
+++ app/components/NavBar/NavLink.tsx
@@ -0,0 +1,55 @@
+/* eslint-disable react/prop-types */
+import { Link } from "@remix-run/react";
+
+import { useTheme } from "~/providers/ThemeProvider";
+import { SupportedTheme } from "~/types";
+
+/**
+ * 1. Light Mode = text-gray-500 hover:text-black;
+ * 2. Dark Mode = text-gray-400 hover:text-white
+ */
+
+const getTextColorClassNameForNavLink = (
+  currentTheme: SupportedTheme
+) => {
+  if (currentTheme === SupportedTheme.DARK) {
+    return "text-gray-400 hover:text-white";
+  }
+
+  if (currentTheme === SupportedTheme.LIGHT) {
+    return "text-gray-400 hover:text-black";
+  }
+};
+
+type NavLinkProps = Omit<Parameters<typeof Link>["0"], "to"> & {
+  to: string;
+} & {
+  isCurrentRoute: boolean;
+};
+
+const NavLink: React.FC<NavLinkProps> = ({
+  to,
+  isCurrentRoute,
+  ...rest
+}) => {
+  const { theme } = useTheme();
+
+  const textColorClassName = getTextColorClassNameForNavLink(
+    theme
+  );
+  const IS_CURRENT_ROUTE_CLASSNAME = "text-navBar-linkActive";
+
+  return (
+    <li className="px-5 py-2">
+      <Link
+        prefetch="intent"
+        className={`focus:outline-none block whitespace-nowrap text-lg font-medium ${textColorClassName} ${isCurrentRoute ? IS_CURRENT_ROUTE_CLASSNAME : null
+          }`}
+        to={to}
+        {...rest}
+      />
+    </li>
+  );
+};
+
+export default NavLink;
