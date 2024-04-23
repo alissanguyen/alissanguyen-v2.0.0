584b5d9eaf1747205f123d2cfa274bf3072c2993
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2024-04-23 15:37:35 -0700

    Add EntryHyperLink and HyperLink components

---
+++ app/components/Contentful/EntryHyperLink/EntryHyperLink.tsx
@@ -0,0 +1,26 @@
+import { Node } from "@contentful/rich-text-types";
+import * as React from "react";
+
+interface Props {
+  node: Node;
+  children: React.ReactNode;
+}
+
+/**
+ * When I publish a blog post and I link to another blog post, this is the component
+ * that is rendered!
+ */
+const EntryHyperLink: React.FC<Props> = (props) => {
+  const otherPostSlug: string = props.node.data.target.fields.blogPostSlug;
+
+  return (
+    <a
+      className="BlogPost__EntryHyperLink text-post-hyperlink underlined hover:text-post-hyperlinkHover italic"
+      href={`/blog/${otherPostSlug}`}
+    >
+      {props.children}
+    </a>
+  );
+};
+
+export default EntryHyperLink;
