83c330b173797580f8aa0ff986f0fde5912228ac
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2024-04-23 15:33:28 -0700

    Add entry.client,tsx

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -7,6 +7,14 @@
 import { RemixBrowser } from "@remix-run/react";
 import { startTransition, StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
+import { hijackEffects } from "stop-runaway-react-effects";
+
+if (process.env.NODE_ENV === "development") {
+  hijackEffects({
+    callCount: 500,
+    timeLimit: 1000
+  });
+}
 
 startTransition(() => {
   hydrateRoot(
